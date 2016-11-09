3cbc64e9b12d6e8b81e6bf143a6bba8524f9a90c
gpcorser <gpcorser@svsu.edu>
2016-11-09 16:08:53 -0500

    through apge 646

--- app/src/main/java/com/example/gpcorser/bitsandpizzas/TopFragment.java
+++ app/src/main/java/com/example/gpcorser/bitsandpizzas/TopFragment.java
@@ -1,11 +1,16 @@
 package com.example.gpcorser.bitsandpizzas;
 
 
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 /**
@@ -13,17 +18,40 @@ import android.view.ViewGroup;
  */
 public class TopFragment extends Fragment {
 
-
     public TopFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        RelativeLayout layout =(RelativeLayout)inflater.inflate(R.layout.fragment_top,  container, false);
+        RecyclerView pizzaRecycler = (RecyclerView)layout.findViewById(R.id.pizza_recycler);
+        String[] pizzaNames = new String[2];
+        for (int i = 0; i < 2; i++) {
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+        }
+        int[] pizzaImages = new int[2];
+        for (int i = 0; i < 2; i++) {
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pizzaRecycler.setLayoutManager(layoutManager);
+        CaptionedImageAdapter adapter = new CaptionedImageAdapter(pizzaNames, pizzaImages);
+        pizzaRecycler.setAdapter(adapter);
+        adapter.setListener(new CaptionedImageAdapter.Listener() {
+            @Override
+            public void onClick(int position) {
+                Intent intent = new Intent(getActivity(), PizzaDetailActivity.class);
+                intent.putExtra(PizzaDetailActivity.EXTRA_PIZZANO, position);
+                getActivity().startActivity(intent);
+            }
+        });
+        return layout;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top, container, false);
+        //return inflater.inflate(R.layout.fragment_top, container, false);
     }
 
 }
